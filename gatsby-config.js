5126493051aac3bd7e1b948e1003ddb99f9a26bc
Sean Groff <sean.groff@selectquote.com>
2019-05-20 16:07:19 -0500

    fix plugin feed icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
         background_color: `#f8f7f4`,
         theme_color: `#007aff`,
         display: `minimal-ui`,
-        icon: `src/assets/gatsby-icon.png`,
+        icon: `src/static/favicon.ico`,
       },
     },
     `gatsby-plugin-offline`,
