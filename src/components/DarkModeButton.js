efbf7c6e7871a50017d31de22f2b4fd4860573c3
Sean Groff <sgroff04@gmail.com>
2020-01-20 23:50:21 -0600

    dont even know

--- src/components/DarkModeButton.js
+++ src/components/DarkModeButton.js
@@ -12,7 +12,7 @@ function DarkModeButton() {
   return (
     <span>
       <Toggle
-        defaultChecked={darkMode.value}
+        defaultChecked={darkMode.value || true}
         aria-label="Dark Mode Toggle"
         icons={{ checked: <IconCool />, unchecked: <IconGhost /> }}
         onChange={darkMode.toggle}
