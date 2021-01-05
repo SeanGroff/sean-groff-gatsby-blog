6326e9223231dcd162125c50506fae336ed03ec3
Sean Groff <sgroff04@gmail.com>
2021-01-04 22:53:48 -0600

    Twitter share for blog

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,6 +1,14 @@
-import { GetStaticPaths, GetStaticProps } from 'next'
+import Head from 'next/head'
 import hydrate from 'next-mdx-remote/hydrate'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { TwitterShareButton } from 'react-share'
 import { getAllPostSlugs, getPostData } from '../../src/lib/posts'
+import {
+  baseUrl,
+  cloudinaryUrl,
+  myFullName,
+  twitterUsername,
+} from '../../src/constants'
 import type { Post as PostType } from '../../src/types'
 
 type Props = {
@@ -9,7 +17,38 @@ type Props = {
 
 function Post({ post }: Props) {
   const source = hydrate(post.source)
-  return <div>{source}</div>
+  const title = post.frontMatter.title
+  const titleAndAuthor = `${title} by ${myFullName} ${twitterUsername}`
+  const description = post.frontMatter.description
+  const featuredImage = post.frontMatter?.featuredImage
+  const cloudinaryImageUrl = `${cloudinaryUrl}/${featuredImage}`
+
+  return (
+    <div>
+      <Head>
+        <title>{titleAndAuthor}</title>
+        <meta name="description" content={description} />
+        <meta name="image" content={cloudinaryImageUrl} />
+
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content={twitterUsername} />
+        <meta name="twitter:title" content={titleAndAuthor} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={cloudinaryImageUrl} />
+      </Head>
+      {source}
+      <div style={{ marginTop: 16 }}>
+        {/* TODO: Test Share button in Production */}
+        <TwitterShareButton
+          title={titleAndAuthor}
+          url={`${baseUrl}${post.slug}`}
+        >
+          Click here to share this article on Twitter ❤️
+        </TwitterShareButton>
+      </div>
+    </div>
+  )
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export const baseUrl = 'https://www.seangroff.dev'
+export const twitterUsername = '@_SeanGroff'
+export const myFullName = 'Sean Groff'
+export const cloudinaryUrl = 'https://res.cloudinary.com/seangroff'
